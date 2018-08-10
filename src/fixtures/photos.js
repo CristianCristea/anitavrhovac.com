b045f671eaea5780d0f7b657869c52c89bfffef2
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-10 14:47:20 +0200

    Add dummy data

---
+++ src/fixtures/photos.js
@@ -0,0 +1,88 @@
+export const photos = {
+  random_photo_id_1: {
+    collection: 'random_album_1',
+    photo_description: 'test description',
+    urls: {
+      full: `${process.env.PUBLIC_URL}/images/random_photo_id_1/food.jpg`,
+      regular: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_1/food-regular.jpg`,
+      small: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_1/food-small.jpg`,
+      thumb: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_1/food-thumbnail.jpg`
+    },
+    tags: { clouds: true, sky: true },
+    likes: 12,
+    liked_by_admin: true,
+    created_at: Date.now()
+  },
+  random_photo_id_2: {
+    collection: 'random_album_1',
+    photo_description: 'test description',
+    urls: {
+      full: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_2/edgar-castrejon-459807-unsplash.jpg`,
+      regular: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_2/edgar-castrejon-459807-unsplash-regular.jpg`,
+      small: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_2/edgar-castrejon-459807-unsplash-small.jpg`,
+      thumb: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_2/edgar-castrejon-459807-unsplash-thumbnail.jpg`
+    },
+    tags: { clouds: true, sky: true },
+    likes: 12,
+    liked_by_admin: true,
+    created_at: Date.now()
+  },
+  random_photo_id_3: {
+    collection: 'random_album_1',
+    photo_description: 'test description',
+    urls: {
+      full: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_3/cel-lisboa-60315-unsplash.jpg`,
+      regular: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_3/cel-lisboa-60315-unsplash-regular.jpg`,
+      small: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_3/cel-lisboa-60315-unsplash-small.jpg`,
+      thumb: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_3/cel-lisboa-60315-unsplash-thumbnail.jpg`
+    },
+    tags: { clouds: true, sky: true },
+    likes: 12,
+    liked_by_admin: true,
+    created_at: Date.now()
+  },
+  random_photo_id_4: {
+    collection: 'random_album_1',
+    photo_description: 'test description',
+    urls: {
+      full: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_4/brooke-lark-229136-unsplash.jpg`,
+      regular: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_4/brooke-lark-229136-unsplash-regular.jpg`,
+      small: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_4/brooke-lark-229136-unsplash-small.jpg`,
+      thumb: `${
+        process.env.PUBLIC_URL
+      }/images/random_photo_id_4/brooke-lark-229136-unsplash-thumbnail.jpg`
+    },
+    tags: { clouds: true, sky: true },
+    likes: 12,
+    liked_by_admin: true,
+    created_at: Date.now()
+  }
+};
