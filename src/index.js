d463f2699d64382354aa6ad6c7f0134ec828a391
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-10 14:50:16 +0200

    Fix import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './index.css';
-import AppRouter from './AppRouter';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
