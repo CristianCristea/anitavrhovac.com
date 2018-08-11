0c9942f98ee57ee4cc8b12efb6648179414a86cc
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-11 18:22:32 +0200

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import './index.css';
 import AppRouter from './components/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
