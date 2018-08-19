b07045c9d0c682f73f8487b09514f12b8882e053
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-19 12:39:36 +0200

    Add, delete, edit, album

---
+++ src/components/Admin/Album/Form/index.js
@@ -0,0 +1,224 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import uuid from 'uuid';
+import { connect } from 'react-redux';
+import { addAlbum, editAlbum } from './../../../../actions/albums';
+import FormErrors from './../../../FormErrors';
+import './AlbumForm.css';
+
+let AlbumForm = class extends Component {
+  state = {
+    // album
+    name: this.props.album.name,
+    description: this.props.album.description,
+    location: this.props.album.location,
+    publicAlbum: false,
+    cover: {
+      sizes: {
+        full: `${process.env.PUBLIC_URL}/images/image-placeholder.jpg`
+      }
+    },
+    // form validation
+    formErrors: {
+      name: '',
+      description: '',
+      location: ''
+    },
+    nameValid: false,
+    descriptionValid: false,
+    locationValid: false,
+    formValid: false
+  };
+
+  componentDidMount() {
+    this.editAlbumInitialValidation();
+  }
+
+  handleTextInput = e => {
+    const fieldName = e.currentTarget.name;
+    const filedValue = e.currentTarget.value;
+
+    this.setState({ [fieldName]: filedValue }, () => {
+      this.validateField(fieldName, filedValue);
+    });
+  };
+
+  // on edit album, set album as valid
+  editAlbumInitialValidation = () => {
+    this.setState(
+      {
+        nameValid: true,
+        descriptionValid: true,
+        locationValid: true
+      },
+      this.validateForm
+    );
+  };
+
+  validateField(fieldName, value) {
+    let {
+      formErrors: fieldValidationErrors,
+      nameValid,
+      descriptionValid,
+      locationValid,
+      selectedCoverValid
+    } = this.state;
+
+    const fileTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/pjpeg'];
+
+    switch (fieldName) {
+      case 'name':
+        nameValid = !!(value.match(/[a-z]/gi) && value.length > 3);
+        fieldValidationErrors.name = nameValid ? '' : ' is invalid';
+        break;
+      case 'description':
+        descriptionValid = !!(value.match(/[a-z]/gi) && value.length > 3);
+        fieldValidationErrors.description = descriptionValid
+          ? ''
+          : ' is invalid';
+        break;
+      case 'location':
+        locationValid = !!(value.match(/[a-z]/gi) && value.length > 3);
+        fieldValidationErrors.location = locationValid ? '' : ' is invalid';
+        break;
+      case 'selectedCover':
+        // check image type and size < 3MB
+        selectedCoverValid =
+          fileTypes.indexOf(value.type) !== -1 &&
+          (value.size / 1048576).toFixed(1) < 3;
+        fieldValidationErrors['cover photo'] = selectedCoverValid
+          ? ''
+          : ' is invalid';
+        break;
+      default:
+        break;
+    }
+
+    this.setState(
+      {
+        formErrors: fieldValidationErrors,
+        nameValid: nameValid,
+        descriptionValid: descriptionValid,
+        locationValid: locationValid
+      },
+      () => this.validateForm()
+    );
+  }
+
+  validateForm() {
+    this.setState({
+      formValid:
+        this.state.nameValid &&
+        this.state.descriptionValid &&
+        this.state.locationValid
+    });
+  }
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+
+    const { edit, album: albumToEdit } = this.props;
+    const { name, description, location, cover, publicAlbum } = this.state;
+    let editCover = !edit
+      ? cover
+      : { sizes: { full: albumToEdit.cover.sizes.full } };
+    let albumId = !edit ? uuid() : albumToEdit.id;
+    const album = {
+      id: albumId,
+      created_at: Date.now(),
+      cover: editCover,
+      name,
+      description,
+      location,
+      photos: [],
+      publicAlbum
+    };
+
+    // update the state
+    // this.props.createAlbum(album);
+    if (!edit) {
+      this.props.dispatch(addAlbum(album));
+    } else {
+      this.props.dispatch(editAlbum(album.id, album));
+    }
+
+    // redirect to admin dashboard
+    this.props.history.push(`${process.env.PUBLIC_URL}/anita/dashboard`);
+
+    // reset form
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      description: '',
+      location: ''
+    });
+  };
+
+  // add has-error css class if the field has an error
+  hasError(error) {
+    return error.length === 0 ? '' : 'has-error';
+  }
+
+  render() {
+    return (
+      // render form validation errors
+      <div className="add-album">
+        <FormErrors formErrors={this.state.formErrors} />
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            className={`${this.hasError(this.state.formErrors.name)}`}
+            type="text"
+            name="name"
+            placeholder="name"
+            required
+            value={this.state.name}
+            onChange={this.handleTextInput}
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="description"
+            required
+            value={this.state.description}
+            onChange={this.handleTextInput}
+          />
+          <input
+            type="text"
+            name="location"
+            placeholder="location"
+            required
+            value={this.state.location}
+            onChange={this.handleTextInput}
+          />
+
+          <button type="submit" disabled={!this.state.formValid}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+};
+
+// connect to store for event dispatch
+AlbumForm = connect()(AlbumForm);
+export default AlbumForm;
+
+AlbumForm.propTypes = {
+  edit: PropTypes.bool
+};
+
+AlbumForm.defaultProps = {
+  edit: false,
+  album: {
+    name: '',
+    description: '',
+    location: ''
+  }
+};
+
+// user input: name, description, location, cover - photo-file
+// generate: id, created_at,
