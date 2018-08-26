d60b3a6285a7fa3b1bb91b2c2f7727b87286b822
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-26 14:59:13 +0200

    Refactor routes - for connected components

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { collections, photos } from './../fixtures';
 import { Navbar } from './../components/Layout/';
 import Albums from './../components/Pages/Albums';
 import Album from './../components/Pages/Albums/Album';
 import Homepage from './../components/Pages/Homepage';
 import PageNotFound from './../components/Pages/PageNotFound';
-import Photo from './../components/Pages/Photo';
 import About from './../components/Pages/About';
 import AdminAddPhoto from './../components/Admin/Photo/AddPhoto';
 import AdminAlbum from './../components/Admin/Album';
@@ -15,24 +14,21 @@ import AdminAddAlbum from './../components/Admin/Album/AddAlbum';
 import AdminPhoto from './../components/Admin/Photo';
 import AdminDashboard from './../components/Admin/Dashboard';
 
-class AppRouter extends Component {
-  state = {
-    collections,
-    photos
-  };
-
-  createAlbum = album => {
-    this.setState(prevState => {
-      return { collections: prevState.collections.concat(album) };
-    });
-  };
-
+let AppRouter = class extends Component {
   render() {
     // sort all photos desc
-    const latestPhotos = this.state.photos.sort((photo, nextPhoto) => {
+    const latestPhotos = this.props.photos.sort((photo, nextPhoto) => {
       return nextPhoto.created_at - photo.created_at;
     });
-    const { collections: albums } = this.state;
+    const { publicAlbums } = this.props;
+    const latestPublicPhotos = latestPhotos.filter(photo => {
+      for (let i = 0; i < publicAlbums.length; i++) {
+        if (photo.album.id === publicAlbums[i].id) {
+          return photo;
+        }
+      }
+      return false;
+    });
 
     return (
       <BrowserRouter>
@@ -42,26 +38,17 @@ class AppRouter extends Component {
             <Route
               exact
               path={`${process.env.PUBLIC_URL}/`}
-              render={() => <Homepage latestPhotos={latestPhotos} />}
+              render={() => <Homepage latestPhotos={latestPublicPhotos} />}
             />
             <Route
               exact
               path={`${process.env.PUBLIC_URL}/albums`}
-              render={props => <Albums albums={albums} {...props} />}
+              component={Albums}
             />
             <Route
-              path={`${process.env.PUBLIC_URL}/albums/:id`}
-              render={params => <Album albums={albums} {...params} />}
-            />
-            <Route
-              path={`${process.env.PUBLIC_URL}/photos/:id`}
-              render={params => (
-                <Photo
-                  photos={this.state.photos}
-                  albums={this.state.collections}
-                  {...params}
-                />
-              )}
+              exact
+              path={`${process.env.PUBLIC_URL}/albums/:album_id`}
+              component={Album}
             />
             <Route path={`${process.env.PUBLIC_URL}/about`} component={About} />
             <Route
@@ -91,12 +78,24 @@ class AppRouter extends Component {
               path={`${process.env.PUBLIC_URL}/anita/:album_id/add-photo`}
               component={AdminAddPhoto}
             />
+            <Route
+              exact
+              path={`${process.env.PUBLIC_URL}/:album_id/:photo_id`}
+              component={Album}
+            />
             <Route component={PageNotFound} />
           </Switch>
         </CssBaseline>
       </BrowserRouter>
     );
   }
-}
+};
 
+const mapStateToProps = state => ({
+  collections: state.collections,
+  publicAlbums: state.collections.filter(album => album.publicAlbum),
+  photos: state.photos
+});
+
+AppRouter = connect(mapStateToProps)(AppRouter);
 export default AppRouter;
