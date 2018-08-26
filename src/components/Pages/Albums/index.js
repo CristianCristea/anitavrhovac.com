9eb2d2c41e45746b1a55637648a3a2f6c57fb9cd
CristianCristea <cristiancristea.1987@gmail.com>
2018-08-26 14:55:52 +0200

    Connect to store, display info if no albums

--- src/components/Pages/Albums/index.js
+++ src/components/Pages/Albums/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import AlbumCover from './AlbumCover';
+// display only if published
+let Albums = ({ albums }) => {
+  const noAlbums = (
+    <div>
+      <h3>No albums added</h3>
+      <Link to={`${process.env.PUBLIC_URL}/`}>Back</Link>
+    </div>
+  );
+  const publicAlbums = albums.filter(album => album.publicAlbum);
 
-const Albums = ({ albums }) => {
   return (
     <section className="latest-photos">
       <h2>Albums</h2>
-      {albums.map(album => {
-        return <AlbumCover key={album.id} album={album} />;
-      })}
+      {publicAlbums.length === 0
+        ? noAlbums
+        : publicAlbums.map(album => (
+            <AlbumCover key={album.id} album={album} />
+          ))}
     </section>
   );
 };
 
+const mapStateToProps = state => ({
+  albums: state.collections
+});
+
+Albums = connect(mapStateToProps)(Albums);
 export default Albums;
