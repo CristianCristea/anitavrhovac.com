026159f67bb60660cc307a5424eca402bbdc1e51
CristianCristea <cristiancristea.1987@gmail.com>
2018-09-17 11:27:29 +0200

    Connect to store

--- src/components/Pages/Homepage/index.js
+++ src/components/Pages/Homepage/index.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
 import Photos from './../../Photos';
+import Typography from '@material-ui/core/Typography';
+import './Homepage.css';
 
-const Homepage = ({ latestPhotos }) => {
+let Homepage = ({ latestPhotos, publicAlbums }) => {
+  // display only public photos
+  const latestPublicPhotos = latestPhotos.filter(photo => {
+    for (let i = 0; i < publicAlbums.length; i++) {
+      if (photo.album.id === publicAlbums[i].id) {
+        return photo;
+      }
+    }
+    return false;
+  });
+
   return (
-    <section className="latest-photos">
-      <h2>Homepage</h2>
-      <Photos photos={latestPhotos} />
-    </section>
+    <Grid container spacing={8} className="latest-photos" justify="flex-start">
+      <Grid item xs={12}>
+        <Typography variant="display2" className="latest-photos__headline">
+          Homepage
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Photos photos={latestPublicPhotos} />
+      </Grid>
+    </Grid>
   );
 };
+
+const mapStateToProps = state => ({
+  // sort all photos desc
+  latestPhotos: state.photos.sort((photo, nextPhoto) => {
+    return nextPhoto.created_at - photo.created_at;
+  }),
+  publicAlbums: state.collections.filter(album => album.publicAlbum)
+});
+Homepage = connect(mapStateToProps)(Homepage);
 
 export default Homepage;
