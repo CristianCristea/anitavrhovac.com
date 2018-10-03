a9cd60ec0fe21f82c75216da2c925f194bae969d
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-10-03 13:12:14 +0200

    add redux-thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,9 +3,7 @@ import thunk from 'redux-thunk';
 import { albumsReducer } from './../reducers/albums';
 import { photosReducer } from './../reducers/photos';
 
-const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
     combineReducers({
