c9f02f44c7fa9ef46de4e47fc348c2cd3700b505
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-10-14 13:40:16 +0200

    Add comments

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,14 +1,17 @@
 import { firebase, emailAuthProvider } from './../firebase/firebase';
 
+// store user id in redux store
 export const login = uid => ({
   type: 'LOGIN',
   uid
 });
 
+// remove user id from store
 export const logout = () => ({
   type: 'LOGOUT'
 });
 
+// login user
 export const startLogin = (email, password) => {
   return () => {
     return firebase
@@ -27,6 +30,7 @@ export const startLogin = (email, password) => {
   };
 };
 
+// logout user
 export const startLogout = () => {
   return () => {
     return firebase.auth().signOut();
