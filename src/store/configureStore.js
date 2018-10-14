b0cedef4df0688b5f46b5766396b0b6da249cdd9
Cristian Cristea <cristiancristea.1987@gmail.com>
2018-10-14 13:42:18 +0200

    Add auth reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,16 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { albumsReducer } from './../reducers/albums';
-import { photosReducer } from './../reducers/photos';
+import albumsReducer from './../reducers/albums';
+import photosReducer from './../reducers/photos';
+import authReducer from './../reducers/auth';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
     combineReducers({
       collections: albumsReducer,
-      photos: photosReducer
+      photos: photosReducer,
+      auth: authReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
