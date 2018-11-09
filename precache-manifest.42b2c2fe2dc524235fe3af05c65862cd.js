f9df2f399d878a9ed264ef97e3deb58efcbf594b
CristianCristea <cristiancristea.1987@gmail.com>
2018-11-09 12:40:10 +0100

    Updates

---
+++ precache-manifest.42b2c2fe2dc524235fe3af05c65862cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf42f8f119c6b6e6545c",
+    "url": "/anitavrhovac/static/js/runtime~main.bf42f8f1.js"
+  },
+  {
+    "revision": "593751ee444bd04eed34",
+    "url": "/anitavrhovac/static/js/main.593751ee.chunk.js"
+  },
+  {
+    "revision": "98e943edec41f384e3e0",
+    "url": "/anitavrhovac/static/js/1.98e943ed.chunk.js"
+  },
+  {
+    "revision": "593751ee444bd04eed34",
+    "url": "/anitavrhovac/static/css/main.7095ebd1.chunk.css"
+  },
+  {
+    "revision": "044e84250ad922ae8622f73e2df2342b",
+    "url": "/anitavrhovac/index.html"
+  }
+];
